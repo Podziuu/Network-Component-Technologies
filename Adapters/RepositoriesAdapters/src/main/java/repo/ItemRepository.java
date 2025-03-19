051750d6c6764a90c880ac6011038f0846ee62e3
Piotr Lewkowicz <piotrek.lewkowicz@wp.pl>
2025-03-19 23:54:11 +0100

    TODO add mappers and dtos to adapters

---
+++ Adapters/RepositoriesAdapters/src/main/java/repo/ItemRepository.java
@@ -0,0 +1,68 @@
+package repo;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.result.InsertOneResult;
+import entities.item.ItemEnt;
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@ApplicationScope
+public class ItemRepository extends AbstractMongoEntity {
+    private final MongoCollection<ItemEnt> itemCollection;
+
+    public ItemRepository() {
+        initDbConnection();
+        this.itemCollection = database.getCollection("items", ItemEnt.class);
+    }
+
+    public ObjectId addItem(ItemEnt item) {
+        InsertOneResult result = itemCollection.insertOne(item);
+        item.setId(String.valueOf(result.getInsertedId().asObjectId().getValue()));
+        return result.getInsertedId().asObjectId().getValue();
+    }
+
+    public ItemEnt getItemById(String id) {
+        ObjectId objectId = new ObjectId(id);
+        return itemCollection.find(Filters.eq("_id", objectId)).first();
+    }
+
+    public List<ItemEnt> getItemsByBasePrice(int basePrice) {
+        return itemCollection.find(Filters.eq("basePrice", basePrice)).into(new ArrayList<>());
+    }
+
+    public List<ItemEnt> getItemsByItemName(String itemName) {
+        return itemCollection.find(Filters.eq("itemName", itemName)).into(new ArrayList<>());
+    }
+
+    public List<ItemEnt> getItemsByItemType(String itemType) {
+        return itemCollection.find(Filters.eq("itemType", itemType)).into(new ArrayList<>());
+    }
+
+    public void updateItem(ItemEnt item) {
+        BasicDBObject object = new BasicDBObject();
+        object.put("_id", item.getId());
+        itemCollection.replaceOne(object, item);
+    }
+
+    public void removeItem(ObjectId id) {
+        BasicDBObject object = new BasicDBObject();
+        object.put("_id", id);
+        itemCollection.deleteOne(object);
+    }
+
+    @Override
+    public void close() throws Exception {
+        mongoClient.close();
+    }
+
+    public List<ItemEnt> getAllItems() {
+        return itemCollection.find().into(new ArrayList<>());
+    }
+}
---
+++ Adapters/RepositoriesAdapters/src/main/java/repo/RentRepository.java
@@ -0,0 +1,94 @@
+package repo;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.result.InsertOneResult;
+import entities.RentEnt;
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RentRepository extends AbstractMongoEntity {
+    private final MongoCollection<RentEnt> rentCollection;
+
+    public RentRepository() {
+        initDbConnection();
+        this.rentCollection = database.getCollection("rents", RentEnt.class);
+    }
+
+    public ObjectId addRent(RentEnt rent) {
+        InsertOneResult result = rentCollection.insertOne(rent);
+        rent.setId(String.valueOf(result.getInsertedId().asObjectId().getValue()));
+        return result.getInsertedId().asObjectId().getValue();
+    }
+
+    public RentEnt getRent(String id) {
+        ObjectId objectId = new ObjectId(id);
+        return rentCollection.find(Filters.eq("_id", objectId)).first();
+    }
+
+    public void updateRent(RentEnt rent) {
+        BasicDBObject object = new BasicDBObject();
+        object.put("_id", rent.getId());
+        rentCollection.replaceOne(object, rent);
+    }
+
+    public List<RentEnt> findActiveRents() {
+        return rentCollection.find(Filters.eq("endTime", null)).into(new ArrayList<>());
+    }
+
+    public List<RentEnt> findInactiveRents() {
+        return rentCollection.find(Filters.ne("endTime", null)).into(new ArrayList<>());
+    }
+
+    public List<RentEnt> findRentsByItemId(String itemId) {
+        ObjectId objectId = new ObjectId(itemId);
+        return rentCollection.find(Filters.eq("item._id", objectId)).into(new ArrayList<>());
+    }
+
+    public List<RentEnt> findActiveRentsByItemId(String itemId) {
+        ObjectId objectId = new ObjectId(itemId);
+        return rentCollection.find(Filters.and(
+                Filters.eq("item._id", objectId),
+                Filters.eq("endTime", null)
+        )).into(new ArrayList<>());
+    }
+
+    public List<RentEnt> findInactiveRentsByItemId(String itemId) {
+        ObjectId objectId = new ObjectId(itemId);
+        return rentCollection.find(Filters.and(
+                Filters.eq("item._id", objectId),
+                Filters.ne("endTime", null)
+        )).into(new ArrayList<>());
+    }
+
+    public List<RentEnt> findRentsByClientId(String clientId) {
+        ObjectId objectId = new ObjectId(clientId);
+        return rentCollection.find(Filters.eq("client._id", objectId)).into(new ArrayList<>());
+    }
+
+    public List<RentEnt> findActiveRentsByClientId(String clientId) {
+        ObjectId objectId = new ObjectId(clientId);
+        return rentCollection.find(Filters.and(
+                Filters.eq("client._id", objectId),
+                Filters.eq("endTime", null)
+        )).into(new ArrayList<>());
+    }
+
+    public List<RentEnt> findInactiveRentsByClientId(String clientId) {
+        ObjectId objectId = new ObjectId(clientId);
+        return rentCollection.find(Filters.and(
+                Filters.eq("client._id", objectId),
+                Filters.ne("endTime", null)
+        )).into(new ArrayList<>());
+    }
+
+    @Override
+    public void close() {
+        mongoClient.close();
+    }
+}
